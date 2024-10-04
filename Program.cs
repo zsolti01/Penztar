80d4cc105c3f7bc7a4f23eca2a5bf6431c3997d9
vecseizsolt <vecseizs@kkszki.hu>
2024-10-04 11:33:53 +0200

    Bekér method is ready.

--- Program.cs
+++ Program.cs
@@ -8,9 +8,20 @@ namespace Penztar
 {
     internal class Program
     {
+        public static int[] tm = new int[6] {500, 1000, 2000, 5000, 10000, 20000};
+        public static int[] money = new int[6];
+
+        public static void beker()
+        {
+            for (int i = 0; i < tm.Length; i++)
+            {
+                Console.Write($"Kérem {i+1}. bankjegy értékét: ");
+                money[i] = Convert.ToInt32(Console.ReadLine());
+            }
+        }
         static void Main(string[] args)
         {
-
+            beker();
         }
     }
 }
